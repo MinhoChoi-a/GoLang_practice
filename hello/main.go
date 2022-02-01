511b0b3a2d91ba57582d1ef5ea9db7445abe2149
minhochoi-a <minhocalgary@gmail.com>
2022-01-31 19:54:34 -0700

    Data type and input package

--- hello/main.go
+++ hello/main.go
@@ -4,6 +4,33 @@ import (
 	"fmt"
 )
 
+const constInt int = 1000
+
 func main() {
-	fmt.Println("Hello from Go!")
+
+	var aString string = "This is Go"
+
+	fmt.Println(aString)
+	fmt.Printf("types is %T\n", aString)
+
+	var anInteger int = 42
+
+	fmt.Println(anInteger)
+	fmt.Printf("types is %T\n", anInteger)
+
+	var defaultInt int
+
+	fmt.Println(defaultInt)
+
+	var anotherInt = 100
+
+	fmt.Println(anotherInt)
+	fmt.Printf("types is %T\n", anotherInt)
+
+	//should be inside the function
+	anotherString := "This is also String"
+
+	fmt.Println(anotherString)
+	fmt.Printf("types is %T\n", anotherString)
+
 }
---
+++ practice/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter text: ")
+
+	//underscore: to ignore variable
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Your input: ", input)
+
+	fmt.Print("Enter a number: ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("value of number: ", aFloat)
+	}
+
+	var anInt int = 5
+	var otherFloat float64 = 42
+	sum := float64(anInt) + otherFloat
+	fmt.Print(sum)
+
+}
